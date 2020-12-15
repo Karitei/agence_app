3a4460e2eb52f97d87881e90b1653d96e1d5a189
Sarah HARITI <shariti@allianceassurances.com>
2020-12-15 11:50:30 +0100

    multiauth installed

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -27,6 +28,32 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+
+    public function redirectTo(){
+        switch (Auth::user()->role){
+            case "0":
+                $this->redirectTo ='/admin';
+                return $this->redirectTo;
+
+                break;
+
+            case "1":
+                $this->redirectTo ='/agency';
+                return $this->redirectTo;
+
+                break;
+
+            case "2":
+                $this->redirectTo ='/buisness';
+                return $this->redirectTo;
+
+                break;
+            default:
+                $this->redirectTo = '\login';
+                return $this->redirectTo;
+                break;
+        }
+    }
 
     /**
      * Create a new controller instance.
---
+++ app/Http/Middleware/Buisness.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class Buisness
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(!Auth::check()){
+            return redirect()->route('login');
+        }
+
+        /// Role = 0 => Admin
+        if(Auth::user()->role =="0"){
+            return redirect()->route('admin_dashboard');
+        }
+
+        /// Role = 1 => Agency
+        if(Auth::user()->role =="1"){
+
+            return redirect()->route('agency_dashboard');
+        }
+
+        /// Role = 2 => Buisness
+        if(Auth::user()->role =="2"){
+            return $next($request);
+
+        }
+    }
+}
